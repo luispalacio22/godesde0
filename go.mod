0992ad71f239c6440797121610cdc5478c4a52a7
luispalacio22 <luispalacio11c@gmail.com>
2024-01-31 11:40:49 -0500

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luispalacio22/godesde0
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/luispalacio22/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
