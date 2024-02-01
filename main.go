de60eca5f8a750f333ecfdf15f6fbc0574692d5d
luispalacio22 <luispalacio11c@gmail.com>
2024-02-01 10:57:31 -0500

    commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/luispalacio22/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConvirtiendoTexto(1568)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
